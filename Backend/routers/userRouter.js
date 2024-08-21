bc071f1467185257217ae53b8ad523875ae5dc17
271-noor <157873714+271-noor@users.noreply.github.com>
2024-08-21 23:44:05 +0530

    Added login, logout, and getUser Routes

--- Backend/routers/userRouter.js
+++ Backend/routers/userRouter.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import { login, register } from '../controllers/userController.js';
+import { 
+    getUser,
+    login, 
+    logout, 
+    register, 
+    updateProfile
+} from '../controllers/userController.js';
+import { isAuthenticated } from "../middleware/auth.js"
+
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+router.get("/me", isAuthenticated, getUser);
+router.put("/update/me", isAuthenticated, updateProfile);
 
 export default router;
