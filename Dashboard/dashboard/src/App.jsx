89761c8b1a41be0269e6bfa8b02000bdaa5c6111
271-noor <157873714+271-noor@users.noreply.github.com>
2024-08-30 14:45:28 +0530

    Updated

--- Dashboard/dashboard/src/App.jsx
+++ Dashboard/dashboard/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
@@ -13,6 +14,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useDispatch } from 'react-redux'
 import { getUser } from './store/slices/userSlice'
+import { getAllMessages } from './store/slices/messagesSlice'
 
 
 
@@ -22,6 +24,7 @@ const dispatch = useDispatch()
 
 useEffect(() => {
     dispatch(getUser());
+    dispatch(getAllMessages());
 },[])
 
   return (
